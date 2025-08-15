f2ba4854e5542469433844554561d4896869a952
EnablonBEichinger <126281838+EnablonBEichinger@users.noreply.github.com>
2025-08-15 14:03:41 -0500

    Update Outlook_GatherEventsOnly.ps1
    Logic added to be able to add the StoryPoints based on standard timings.

--- Testing/Outlook_GatherEventsOnly.ps1
+++ Testing/Outlook_GatherEventsOnly.ps1
@@ -34,6 +34,8 @@ foreach ($item in $filteredItems) {
         $end = [datetime]$item.End
         $duration = $end - $start
         $durationFormatted = "{0}h {1}m" -f $duration.Hours, $duration.Minutes
+        $totalMinutes = [math]::Round($duration.TotalMinutes)
+ 
 
         # Add day prefix if subject contains "Daily"
         $dayPrefix = ""
@@ -41,13 +43,28 @@ foreach ($item in $filteredItems) {
             $dayPrefix = "$($start.DayOfWeek) - "
         }
 
+        # Calculate the Story Points
+        if($totalMinutes -le 15){$storypoints = .25}
+            elseif($totalMinutes -le 30){$storypoints = .5}
+            elseif($totalMinutes -le 45){$storypoints = .75}
+            elseif($totalMinutes -le 60){$storypoints = 1}
+            elseif($totalMinutes -le 90){$storypoints = 1.5}
+            elseif($totalMinutes -le 120){$storypoints = 2}
+            elseif($totalMinutes -le 150){$storypoints = 2.5}
+            elseif($totalMinutes -le 180){$storypoints = 3}
+            elseif($totalMinutes -le 210){$storypoints = 3.5}
+            elseif($totalMinutes -le 240){$storypoints = 4}
+            else{$storypoints = 0}
+
         # Create a custom object for each appointment
         $appt = [PSCustomObject]@{
             Subject   = "Week $weekNumber - $dayPrefix$($item.Subject)"
             Start     = $start
             End       = $end
             Duration  = $durationFormatted
+            Minutes   = $totalMinutes
             Location  = $item.Location
+            StoryPts  = $storypoints
         }
 
         # Add to array
